3da3983d3d11fe17b5f190b479f1491852073b41
KoNoLinLie <101704410+KoNoLinLie@users.noreply.github.com>
2024-02-16 10:58:40 +0800

    Update handler.go

--- service/minecraft/handler.go
+++ service/minecraft/handler.go
@@ -29,6 +29,15 @@ var (
 	accessedPlayers                  		  = make(map[string]bool)
 )
 
+func isFirstTime(playerName string) bool {
+	_, exists := accessedPlayers[playerName]
+	if !exists {
+		accessedPlayers[playerName] = true
+		return true
+	}
+	return false
+}
+
 func badPacketPanicRecover(s *config.ConfigProxyService) {
 	// Non-Minecraft packet which uses `go-mc` packet scan method may cause panic.
 	// So a panic handler is needed.
