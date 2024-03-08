2401bdbd50e9d2193731bf26088e68f4bf7b5528
MKyiwuQwQ <111745080+Mengke15@users.noreply.github.com>
2024-03-08 20:28:59 +0800

    Update marshal.go

--- config/marshal.go
+++ config/marshal.go
@@ -2,14 +2,11 @@ package config
 
 import (
 	"encoding/json"
-
-	"github.com/CubeWhyMC/NoDelay-Proxy-Server/common/set"
 )
 
 type configMainTemp struct {
-	Services []*ConfigProxyService
+	Services      []*ConfigProxyService
 	PrivateConfig *Something
-	Lists    map[string][]string
 }
 
 var (
@@ -18,44 +15,21 @@ var (
 )
 
 func (c *configMain) MarshalJSON() ([]byte, error) {
-	var list map[string][]string
-	if l := len(c.Lists); l != 0 {
-		list = make(map[string][]string, l) // map size init
-		for k, v := range c.Lists {
-			list[k] = make([]string, 0, len(v))
-			for k1 := range v {
-				list[k] = append(list[k], k1)
-			}
-		}
-	}
 	return json.Marshal(
 		configMainTemp{
-			Services: c.Services,
+			Services:      c.Services,
 			PrivateConfig: c.PrivateConfig,
-			Lists:    list,
 		},
 	)
 }
 
 func (c *configMain) UnmarshalJSON(data []byte) (err error) {
-	configTemp := configMainTemp{
-		Services: c.Services,
-		PrivateConfig: c.PrivateConfig,
-	}
+	configTemp := configMainTemp{}
 	err = json.Unmarshal(data, &configTemp)
 	if err != nil {
 		return err
 	}
-	// log.Println("Lists:", configTemp.Lists)
-	if l := len(configTemp.Lists); l == 0 { // if nothing in Lists
-		c.Lists = map[string]set.StringSet{} // empty map
-	} else {
-		c.Lists = make(map[string]set.StringSet, l) // map size init
-		for k, v := range configTemp.Lists {
-			// log.Println("List: Loading", k, "value:", v)
-			c.Lists[k] = set.NewStringSetFromSlice(v)
-		}
-	}
+
 	c.Services = configTemp.Services
 	c.PrivateConfig = configTemp.PrivateConfig
 	return nil
